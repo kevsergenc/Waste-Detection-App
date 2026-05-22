898d1c11bafbd6dc8aca45f604625cbf40138362
Kevser Genç <kevsergenc2003@gmail.com>
2026-05-23 00:09:32 +0300

    Update final_pipeline.py

--- waste-app/final_pipeline.py
+++ waste-app/final_pipeline.py
@@ -2,9 +2,10 @@ from ultralytics import YOLO
 import cv2
 import time
 
-model = YOLO(
-    r"C:\Users\Bir Kevser\runs\detect\exp_320-5\weights\best.pt"
-)
+
+genel_model = YOLO(r"C:\Users\Bir Kevser\runs\detect\exp_320-5\weights\best.pt")
+yag_model = YOLO(r"D:\Desktop\Waste-Detection-App-main\yagg - Kopya\runs\detect\train\weights\best.pt")
+
 
 cap = cv2.VideoCapture(0)
 
@@ -12,45 +13,53 @@ if not cap.isOpened():
     print("Kamera açılamadı!")
     exit()
 
+
+def predict(frame):
+
+    result1 = genel_model(frame, conf=0.4)
+
+    if len(result1[0].boxes) > 0:
+        return result1, "Genel Atık"
+
+    result2 = yag_model(frame, conf=0.4)
+
+    if len(result2[0].boxes) > 0:
+        return result2, "Atık Yağ"
+
+    return result1, "Tespit Yok"
+
+
 while True:
 
-    start_time = time.time()
+    start = time.time()
 
     ret, frame = cap.read()
-
     if not ret:
         break
 
-    results = model(
-        frame,
-        imgsz=320,
-        conf=0.4
-    )
+    results, label = predict(frame)
 
-    annotated_frame = results[0].plot()
+    annotated = results[0].plot()
 
-    end_time = time.time()
-
-    fps = 1 / (end_time - start_time)
+    fps = 1 / (time.time() - start)
 
     cv2.putText(
-        annotated_frame,
-        f"FPS: {round(fps,2)}",
-        (20,40),
+        annotated,
+        f"{label} | FPS: {round(fps,2)}",
+        (20, 40),
         cv2.FONT_HERSHEY_SIMPLEX,
         1,
-        (0,255,0),
+        (0, 255, 0),
         2
     )
 
-    cv2.imshow(
-        "AI Waste Detection System",
-        annotated_frame
-    )
+    cv2.imshow("Waste Detection System", annotated)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
 cap.release()
 cv2.destroyAllWindows()
+
 
